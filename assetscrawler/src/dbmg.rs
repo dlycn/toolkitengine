992f81ebb087cdfb762fbbc2b0feb2993806f718
StarsAll_InFrost <StarsAll_InFrost@noreply.gitcode.com>
2026-07-01 19:13:05 +0800

    pass

---
+++ assetscrawler/src/dbmg.rs
@@ -0,0 +1,45 @@
+use rusqlite::{Connection, Params, Result, Statement};
+
+pub struct Table<T> {
+    pub data: T,
+}
+
+impl<T> Table<T> {
+    pub fn new(data: T) -> Self {
+        Self { data }
+    }
+}
+
+pub struct Databasemanager {
+    path: String,
+    conn: Connection,
+}
+
+impl Databasemanager {
+    pub fn dbmeta(&self) -> String {
+        format!("path\t{}", &self.path)
+    }
+
+    pub fn new(path: String, conn: Connection) -> Self {
+        Self { path, conn }
+    }
+
+    fn _desc_execute<P: Params>(&self, sql: &str, params: P, desc: &str) -> Result<usize> {
+        let affected = self.conn.execute(sql, params)?;
+        println!("{}", desc);
+        Ok(affected)
+    }
+
+    pub fn execute<P: Params>(&self, sql: &str, params: P) -> Result<usize> {
+        self._desc_execute(sql, params, "日志")
+    }
+
+    fn _desc_prepare(&self, sql: &str, desc: &str) -> Result<Statement<'_>> {
+        println!("{}", desc);
+        self.conn.prepare(sql)
+    }
+
+    pub fn prepare(&self, sql: &str) -> Result<Statement<'_>> {
+        self._desc_prepare(sql, "游标")
+    }
+}
---
+++ assetscrawler/src/main.rs
@@ -0,0 +1,53 @@
+mod dbmg;
+
+use rusqlite::{Connection, Result};
+use dbmg::{Table,Databasemanager};
+
+#[derive(Debug)]
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>,
+}
+
+fn main() -> Result<()> {
+
+    let path = "./plugins/assetscrawler/test.db";
+    let conn = Connection::open(path)?;
+    let dbm = Databasemanager::new(String::from(path),conn);
+    print!("{}",dbm.dbmeta());
+
+    dbm.execute(
+        "CREATE TABLE person (
+            id   INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            data BLOB
+        )",
+        (), // empty list of parameters.
+    )?;
+    let me = Person {
+        id: 0,
+        name: "Steven".to_string(),
+        data: None,
+    };
+    let table = Table::new(&me);
+    dbm.execute(
+        "INSERT INTO person (name, data) VALUES (?1, ?2)",
+        (&me.name, &me.data),
+    )?;
+    println!("table.data.id:{}",table.data.id);
+
+    let mut stmt = dbm.prepare("SELECT id, name, data FROM person")?;
+    let person_iter = stmt.query_map([], |row| {
+        Ok(Person {
+            id: row.get(0)?,
+            name: row.get(1)?,
+            data: row.get(2)?,
+        })
+    })?;
+
+    for person in person_iter {
+        println!("Found person {:?}", person?);
+    }
+    Ok(())
+}
