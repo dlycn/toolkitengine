5d66737658f8b742759bcf3a6ec6ce58fc692b37
StarsAll_InFrost <StarsAll_InFrost@noreply.gitcode.com>
2026-09-06 08:47:08 +0800

    pass

--- src/entities/pet.rs
+++ src/entities/pet.rs
@@ -1,10 +1,9 @@
 use crate::{
-    components::*, 
-    configs::res,
-    events::pets::ESelectPet,
+    components::*, configs::{res_pet::PET_MAX_SPEED, res_ui::DEPTH_PET}, events::pets::ESelectPet,
 };
 use bevy::image::TextureAtlasTemplate;
 use bevy::prelude::*;
+<<<<<<< HEAD
 use res::pet::*;
 use res::layer::*;
 
@@ -27,6 +26,18 @@ pub fn stdbundle(
             AnimationTimer(Timer::from_seconds(5.0/128.0, TimerMode::Repeating))      
             
     }
+=======
+pub fn new(name: &str, id: u32) -> impl Scene {
+    let path = format!("imgs/Tachies/{}.png", id);
+    bsn! {
+    Cpet{name:{name.to_string()},maxspeed:PET_MAX_SPEED}
+    Cbehavior{direction:Vec2::ZERO,speed:0}
+    Transform::from_xyz(0.0, 0.0, DEPTH_PET)
+    tachie(path)}
+}
+pub fn tachie(path: String) -> impl Scene {
+    bsn! {Sprite{image: path}}
+>>>>>>> parent of e94b3f5 (pass)
 }
 
 pub fn statebundle(text:String) -> impl Scene {
--- src/events/pets.rs
+++ src/events/pets.rs
@@ -1,5 +1,11 @@
 use bevy::prelude::*;
+<<<<<<< HEAD
 use crate::configs::res::pet::*;
+=======
+use bevy::math::bounding::BoundingCircle;
+use crate::components::*;
+use crate::configs::res_pet::*;
+>>>>>>> parent of e94b3f5 (pass)
 
 #[derive(EntityEvent)]
 pub struct ESelectPet{
